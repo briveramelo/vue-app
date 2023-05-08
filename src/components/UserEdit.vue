1fcba412bfbb1f948897cf27eee3a0ef6a6f62de
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2023-05-08 15:17:19 -0500

    editing users now working

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -3,6 +3,68 @@
     <div class="row">
       <div class="col">
         <h1 class="mt-3">User</h1>
+        <hr>
+        <form-tag @userEditEvent="submitHandler" name="userform" event="userEditEvent">
+
+          <text-input
+              v-model="user.first_name"
+              type="text"
+              required="true"
+              label="First Name"
+              :value="user.first_name"
+              name="first-name"
+          ></text-input>
+
+          <text-input
+              v-model="user.last_name"
+              type="text"
+              required="true"
+              label="Last Name"
+              :value="user.last_name"
+              name="last-name"
+          ></text-input>
+
+          <text-input
+              v-model="user.email"
+              type="email"
+              required="true"
+              label="Email"
+              :value="user.email"
+              name="email"
+          ></text-input>
+
+          <!-- New Users -->
+          <text-input
+              v-if="this.user.id === 0"
+              v-model="user.password"
+              type="password"
+              required="true"
+              label="Password"
+              :value="user.password"
+              name="password"
+          ></text-input>
+
+          <!-- Existing Users -->
+          <text-input
+              v-else
+              v-model="user.password"
+              type="password"
+              help="Leave empty to keep existing password"
+              label="Password"
+              :value="user.password"
+              name="password"
+          ></text-input>
+
+          <div class="float-start">
+            <input type="submit" class="btn-primary btn me-2" value="Save">
+            <router-link to="/admin/users" class="btn btn-outline-secondary">Cancel</router-link>
+          </div>
+          <div class="float-end">
+            <a v-if="(this.$route.params.userId > 0) && (parseInt(String(this.$route.params.userId), 10) !== store.user.id)"
+            class="btn btn-danger" href="javascript:void(0);" @click="confirmDelete(this.user.id)"> Delete </a>
+          </div>
+          <div class="clearfix"></div>
+        </form-tag>
       </div>
     </div>
   </div>
@@ -10,10 +72,87 @@
 
 <script>
 import Security from "@/components/security.js"
+import FormTag from "./forms/FormTag.vue"
+import TextInput from "@/components/forms/TextInput.vue";
+import notie from 'notie'
+import {store} from './store.js'
 
 export default {
   beforeMount() {
     Security.requireToken()
+
+    if (parseInt(String(this.$route.params.userId), 10) > 0) {
+      // editing existing user
+      fetch(`${process.env.VUE_APP_API_URL}/admin/users/get/${this.$route.params.userId}`, Security.requestOptions(""))
+          .then(response => response.json())
+          .then(data => {
+            if (data.error) {
+              notie.alert({
+                type: "error",
+                text: data.message,
+              })
+            } else {
+              console.log(data)
+              this.user = data.data.user
+              //we want password to be empty for existing users
+              this.user.password = ""
+              console.log(this.user)
+            }
+          })
+    }
+
+  },
+  data() {
+    return {
+      user: {
+        id: 0,
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+      },
+      store,
+    }
+  },
+  components: {
+    'form-tag': FormTag,
+    'text-input': TextInput,
+  },
+  methods: {
+    submitHandler() {
+      const payload = {
+        id: parseInt(String(this.$route.params.userId), 10),
+        first_name: this.user.first_name,
+        last_name: this.user.last_name,
+        email: this.user.email,
+        password: this.user.password,
+      }
+
+      fetch(`${process.env.VUE_APP_API_URL}/admin/users/save`, Security.requestOptions(payload))
+          .then(response =>response.json)
+          .then(data =>{
+            if (data.error){
+              notie.alert({
+                type:'error',
+                text: data.message
+              })
+            } else {
+              notie.alert({
+                type: 'success',
+                text: 'Changes saved!',
+              })
+            }
+          })
+          .catch(error =>{
+            notie.alert({
+              type:'error',
+              text: error
+            })
+          })
+    },
+    confirmDelete() {
+      //todo: write
+    }
   }
 }
 </script>
